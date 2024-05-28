49cecdfa6b04f001ba64f20e047ea6f7f4ccfbf2
Matheus Vidal <matheusvidal140@gmail.com>
2024-05-28 18:30:25 -0300

    Initial commit

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/matheusvidal21/go-gRPC/internal/database"
+	"github.com/matheusvidal21/go-gRPC/internal/pb"
+	"github.com/matheusvidal21/go-gRPC/internal/service"
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
